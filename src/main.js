8044f657b74485aeca7fbd00f7503605f925d693
ElenaKholodilova <elena.kholodilova06@ya.ru>
2024-01-28 21:22:24 +0300

    functions.Lesson4

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// Функция
+//const{ greet, farewell } = require( "./module.mjs")
+import { greet, farewell, myFunction}  from  "./module.js"
+
+console.log(greet('World'))
+console.log(farewell('World'))
+console.log(myFunction('Elena'))
+
+
+// const check = () => {
+//   console.log('Видно')
+//   return 'результат'
+//   console.log('Не видно') // не выполнится
+// }
+
+// console.log(check())
+
---
+++ src/module.js
@@ -0,0 +1,18 @@
+export function greet(name) {
+  return `Hello, ${name}!`
+}
+
+// Стрелочная функция
+ export const farewell = name => {
+  return `Goodbye, ${name}!`
+}
+
+export function myFunction(name) {
+  return `Первая функция созданная,${name}`
+}
+//  module.exports = {
+//   greet,
+//   farewell
+//  }
+
+
